b2ecfdf7164313c72cfcf39d0aa187f05b726fc7
Steve Malers <steve.malers@openwaterfoundation.org>
2018-03-14 05:46:34 -0600

    Add run-mkdocs-serve-8000.bat to run server in Windows

---
+++ build-util/README.md
@@ -0,0 +1,13 @@
+# build-util #
+
+These scripts are used to view and deploy the MkDocs static website.
+
+* `copy-to-owf-amazon-s3.sh`
+	+ copy static website files in `../site` to OWF Amazon S3 bucket
+	+ **can only be run by user/role with permissions to deploy**
+* `run-mkdocs-serve-8000.bat`
+	+ run the MkDocs 'serve' command to view the site
+	+ **use this in Cygwin, Git Bash, Linux**
+* `run-mkdocs-serve-8000.sh`
+	+ run the MkDocs 'serve' command to view the site
+	+ **use this in Windows command prompt window**
---
+++ build-util/run-mkdocs-serve-8000.bat
@@ -0,0 +1,41 @@
+@echo off
+rem Run mkdocs serve on port 8000 (default)
+rem - this uses 'python' and 'mkdocs' found in the path
+
+rem Make sure that this is being run from the build-util folder
+rem - on Linux could use basename to check the present working directory
+rem - for filename manipulation, see:  https://ss64.com/nt/syntax-args.htmlhttps://ss64.com/nt/syntax-args.html
+echo ================================================================
+echo Running in folder shown below
+echo %cd%
+
+rem Do a simpler test, which should work most of the time
+if not exist "run-mkdocs-serve-8000.bat" (
+	echo Must run from build-util folder.  Exiting.
+	goto end
+)
+
+rem Change to the MkDocs project folder so MkDocs will find the files
+cd ..\mkdocs-project
+
+echo ================================================================
+echo Python found in PATH is used is shown below.
+where python
+echo Mkdocs found in PATH is shown below.
+where mkdocs
+echo ================================================================
+
+echo View the website using http://localhost:8000
+echo Use CTRL-C to stop the server.
+echo ================================================================
+rem mkdocs serve -a 0.0.0.0:8000
+mkdocs serve
+
+rem Changed back to the original folder
+rem - does not seem to work after CTRL-C, probably expected since
+rem   CTRL-C kills this batch file, not just 'mkdocs'
+cd %__pwd%
+
+:end
+rem Exit the batch file but don't close the window
+exit /b
